36ecc223825a116baa1822ba8e1bb2a8e660b438
amaleno <alvaromalenoaferez@gmail.com>
2023-09-29 21:54:48 +0200

    Git Client

---
+++ config-client-restful/src/main/java/com/templates/configclientrestful/ConfigClientRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.templates.configclientrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientRestfulApplication.class, args);
+	}
+
+}
---
+++ config-client-restful/src/test/java/com/templates/configclientrestful/ConfigClientRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.templates.configclientrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
