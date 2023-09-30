23556a8fb6e7d0d39152bab5d11d69febd493240
amaleno <alvaromalenoaferez@gmail.com>
2023-09-30 14:19:06 +0200

    Recharge Config changes on the fly

--- config-client-restful/src/main/java/com/templates/configclientrestful/RateController.java
+++ config-client-restful/src/main/java/com/templates/configclientrestful/RateController.java
@@ -1,10 +1,12 @@
 package com.templates.configclientrestful;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RefreshScope
 @Controller
 public class RateController {
 
@@ -17,11 +19,15 @@ public class RateController {
     @Value("${tollstart}")
     String tollstart;
 
+    @Value("${connstring}")
+    String connstring;
+
     @RequestMapping("/rate")
     public String getRate(Model m) {
         m.addAttribute("rateammount", rate);
         m.addAttribute("lanecount", lanecount);
         m.addAttribute("tollstart", tollstart);
+        m.addAttribute("connstring", connstring);
 
         return "rateview";
     }
